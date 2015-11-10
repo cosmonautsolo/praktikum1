b3f9e6a05e29d199f7e6be4a3adda7e7cf8a6ea9
lbaetz <lbaetz@philnet-pc0036.philnet.ad.uni-koeln.de>
2015-11-10 11:12:14 +0100

    Banana.java added

---
+++ objects/src/objects/Banana.java
@@ -0,0 +1,18 @@
+package objects;
+
+public class Banana {
+
+	String color;
+	Integer weight;
+	
+	
+	
+	String getColor() {
+		return color;
+	}
+	
+	Integer getWeight() {
+		return weight;
+	}
+	
+}
