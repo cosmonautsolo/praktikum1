17ea6074d2e6a0ef9b77ba80d19927b99b7b7d25
lbaetz <lbaetz@philnet-pc0036.philnet.ad.uni-koeln.de>
2015-12-08 11:20:00 +0100

    MultiDim.java added

--- arrays/src/de/uni_koeln/info/java/MultiDim.java
+++ arrays/src/de/uni_koeln/info/java/MultiDim.java
@@ -1,10 +1,47 @@
 package de.uni_koeln.info.java;
 
 public class MultiDim {
+	
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		MultiDim multi = new MultiDim();
+		multi.print2D(multi.multiply2D (6, 6));
 
 	}
+	
+	public int [][] multiply2D (int rows, int columns){
+		
+		int twoDimensional [][] = new int [rows][columns];
+		
+		for (int i=0; i<rows; i++) {
+			
+			for (int j=0; j<columns; j++) {
+				
+				twoDimensional[i][j] = i * j;
+				
+			}
+		}
+		
+		
+		return twoDimensional;
+	}
 
+	public void print2D(int[][] twoDimensional) {
+		
+		for (int i = 0; i < twoDimensional.length; i++) {
+			
+			for (int j = 0; j < twoDimensional[i].length; j++) {
+				
+				System.out.print(twoDimensional[i][j] + "\t");
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+	}
+	
 }
